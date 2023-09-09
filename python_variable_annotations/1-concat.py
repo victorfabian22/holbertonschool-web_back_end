079073a1fe1938646ce652dc460c9f432fb824f2
victorfabian22 <fabianromeroa22@gmail.com>
2023-09-09 22:59:48 +0000

    concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Base annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """function with two parameters with string annotations"""
+    return str1 + str2
