9316932698612b58e019500a5379822f4237a4cd
victorfabian22 <fabianromeroa22@gmail.com>
2023-09-09 22:54:24 +0000

    variable

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Basic annotations - add"""
+
+
+def add(a: float, b: float) -> float:
+    """function with two parameters with float type annotations"""
+    return a + b
