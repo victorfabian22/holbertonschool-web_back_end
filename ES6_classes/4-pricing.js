744b6140af294ebcbd15ebbc601dd224737fe731
Victorfabian22 <fabianromeroa22@gmail.com>
2023-09-24 22:16:21 +0000

    pricing

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value === 'number') {
+      this._amount = value;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (typeof value === 'object') {
+      this._currency = value;
+    } else {
+      throw new TypeError('Currency must be a number');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
